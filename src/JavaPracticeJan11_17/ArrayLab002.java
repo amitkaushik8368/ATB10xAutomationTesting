87ef940eae1df3b9b862f0d744a68412f0f90b81
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-01-12 13:27:26 +0530

    Jan 12 2025 Commit --Fourth commit

---
+++ src/JavaPracticeJan11_17/ArrayLab002.java
@@ -0,0 +1,16 @@
+package JavaPracticeJan11_17;
+
+import java.util.Arrays;
+
+public class ArrayLab002 {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 9, 3, 4, 10, 1};
+        for (int i = 0, j = arr.length-1; i<j; i++, j--){
+            int temp;
+            temp  = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
