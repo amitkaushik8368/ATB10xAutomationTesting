19e06f237e083bde2970d2f7c91ca594c33b0b4e
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-01-12 10:42:37 +0530

    Jan 12 2025 Commit

---
+++ src/JavaPracticeJan11_17/TableOf9.java
@@ -0,0 +1,32 @@
+package JavaPracticeJan11_17;
+
+public class TableOf9 {
+    public static void main(String[] args) {
+
+        System.out.printf("9x1=%d", 9*1);
+        System.out.println();
+        System.out.printf("9x2=%d", 9*2);
+        System.out.println();
+        System.out.printf("9x3=%d", 9*3);
+        System.out.println();
+        System.out.printf("9x4=%d", 9*4);
+        System.out.println();
+        System.out.printf("9x5=%d", 9*5);
+        System.out.println();
+        System.out.printf("9x6=%d", 9*6);
+        System.out.println();
+        System.out.printf("9x7=%d", 9*7);
+        System.out.println();
+        System.out.printf("9x8=%d", 9*8);
+        System.out.println();
+        System.out.printf("9x9=%d", 9*9);
+        System.out.println();
+        System.out.printf("9x10=%d", 9*10);
+        System.out.println();
+
+        //        for (int i = 1; i <=10; i++){
+//            System.out.printf("9x%d=%d", i, 9*i);
+//            System.out.println();
+//        }
+    }
+}
