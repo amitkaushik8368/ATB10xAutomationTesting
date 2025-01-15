4ca1a08ced4588ffa73efa222f98abdf6adb2729
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-01-15 10:13:05 +0530

    This is my different commit method

--- src/JavaPracticeJan11_17/ArrayLab003.java
+++ src/JavaPracticeJan11_17/ArrayLab003.java
@@ -1,24 +1,29 @@
 package JavaPracticeJan11_17;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayLab003 {
     public static void main(String[] args) {
-        int[] array = {4, 6, 2, 8, 12, 2, 6, 6, 6};
+        int[] array = {4, 6, 2, 8, 12, 2, 6, 6, 6, 12, 12, 12, 4, 4, 4, 8, 8, 8, 8, 8, 8, 8, 2, 2, 2, 2, 2, 10, 10};
         boolean[] decide = new boolean[array.length];
         int[] duplicate = new int[array.length];
+        ArrayList<Integer> duplicateList = new ArrayList<>();
+        //System.out.println(duplicateList);
         for (int i = 0; i<array.length;i++){
             if (decide[i]) continue;
+            int count = 0;
             for (int j =i+1; j< array.length; j++){
                 if (array[i] == array[j]){
-                    duplicate[i] = array[i];
+                    count++;
+                    if (count ==1) duplicateList.add(array[i]);
                     decide[j] = true;
 
                 }
             }
         }
-        System.out.println(Arrays.toString(duplicate));
+        System.out.println(duplicateList);
         System.out.println(Arrays.toString(decide));
     }
 }
