a279b4917c8dbd886977cee52db075af18711d76
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-01-23 09:59:26 +0530

    Learning XPath basics

---
+++ src/JavaPracticeJan18_24/X_Path2.java
@@ -0,0 +1,8 @@
+package JavaPracticeJan18_24;
+
+public class X_Path2 {
+    public static void main(String[] args) {
+        System.out.println("/html/body/div[1]/div[2]/div/div/div/div/form/div[6]/div[1]/button");
+    }
+
+}
