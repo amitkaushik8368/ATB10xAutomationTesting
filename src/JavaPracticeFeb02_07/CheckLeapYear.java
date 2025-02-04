89c9041970c5fee4ec488c7353afebbe6e84221d
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-02-04 22:28:46 +0530

    Feb 04 2025 2nd Commit

---
+++ src/JavaPracticeFeb02_07/CheckLeapYear.java
@@ -0,0 +1,24 @@
+package JavaPracticeFeb02_07;
+
+import java.util.Scanner;
+
+/*
+*
+* Check if the year is a leap year
+* Input: give me the year ->Int
+* Output: Leap year/Not Leap Year -> String
+* Rough Logic
+* IF year % 400==0 -> leap year || (year %4 ==0 && year %100!=0(
+*
+ */
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int year = scanner.nextInt();
+        if(year % 400 == 0 || (year %4 == 0 && year % 100 !=0)){
+            System.out.println("Leap Year");
+        }
+        else System.out.println("Not a leap year");
+    }
+}
---
+++ src/JavaPracticeFeb02_07/GreatestAmongThree.java
@@ -0,0 +1,10 @@
+package JavaPracticeFeb02_07;
+// Program to find the greatest number among three
+public class GreatestAmongThree {
+    public static void main(String[] args) {
+        int x = 10, y = 5, z = 87;
+        System.out.println(x>y?x>z?x:z:y>z?y:z);
+    }
+
+
+}
