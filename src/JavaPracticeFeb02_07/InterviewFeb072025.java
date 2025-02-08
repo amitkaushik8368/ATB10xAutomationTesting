262f6f0f53bffb9e0eec66b258588eb188e202d7
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-02-08 11:30:14 +0530

    Feb 08 2025 1st Commit

--- src/JavaPracticeFeb02_07/InterviewFeb072025.java
+++ src/JavaPracticeFeb02_07/InterviewFeb072025.java
@@ -9,26 +9,30 @@ package JavaPracticeFeb02_07;
 
 public class InterviewFeb072025 {
     public static void main(String[] args) {
-        String input = "Amit Sharma is an Automation Tester";
+        String input = "banana**";
+        String lowerInput = input.toLowerCase();
+        System.out.println(lowerInput);
         boolean[] decide = new boolean[input.length()];
         int len = input.length();
         for (int i =0; i<len; i++)
         {
+            if(lowerInput.charAt(i) == ' ')continue;
             if (decide[i]) continue;
             char count ='1';
+
             for (int j = i+1; j < len; j++)
             {
-                if (input.toLowerCase().charAt(i) == input.toLowerCase().charAt(j))
+                if (lowerInput.charAt(i) == lowerInput.charAt(j))
                 {
                     count++;
                     decide[j] = true;
                 }
             }
 
-            input = input.replace(input.charAt(i), count);
+            lowerInput = lowerInput.replace(lowerInput.charAt(i), count);
 
         }
-        System.out.println(input);
+        System.out.println(lowerInput);
 
     }
 }
