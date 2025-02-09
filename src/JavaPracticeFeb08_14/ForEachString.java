d08f88bd207971a7c6c5ec779f6f05c9c213e805
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-02-09 09:59:49 +0530

    Feb 09 2025 2nd Commit

---
+++ src/JavaPracticeFeb08_14/ForEachString.java
@@ -0,0 +1,10 @@
+package JavaPracticeFeb08_14;
+
+public class ForEachString {
+    public static void main(String[] args) {
+        String name = "Amit Sharma";
+      //  for(char ch : name){               Cannot be used only applicable to arrays and collection framework
+       //     System.out.println(ch);
+        //}
+    }
+}
