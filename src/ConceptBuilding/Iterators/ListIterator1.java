65455bbd56c46b9373f48c80102c68f59dab1012
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-02-25 22:40:34 +0530

    Feb 25 2025 Evening Commit

---
+++ src/ConceptBuilding/Iterators/ListIterator1.java
@@ -0,0 +1,21 @@
+package ConceptBuilding.Iterators;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIterator1 {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+
+        ListIterator<Integer> listIterator = list.listIterator();
+        System.out.println(listIterator.hasPrevious());
+        System.out.println(listIterator.hasNext());
+        System.out.println(listIterator.next());
+        System.out.println(listIterator.next());
+        //System.out.println(listIterator.nextIndex());
+
+        System.out.println("-----------");
+        System.out.println(listIterator.previous());
+
+    }
+}
