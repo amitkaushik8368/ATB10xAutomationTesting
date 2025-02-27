98f29f636981d44679c258af40c0d3e6650ad4fc
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-02-27 09:39:57 +0530

    Feb 27 2025 Morning Commit

---
+++ src/LogicBuilding/Feb22_28/PrintingCharacterArray.java
@@ -0,0 +1,10 @@
+package LogicBuilding.Feb22_28;
+
+public class PrintingCharacterArray
+{
+    public static void main(String[] args) {
+        String str = "Sharma";
+        char[] ch = str.toCharArray();
+        System.out.println(ch);
+    }
+}
