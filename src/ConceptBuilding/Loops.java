df956673478677bcd9f336907782c78c1fe06708
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-03-01 22:24:15 +0530

    01 March 2025 Night Commit

---
+++ src/ConceptBuilding/Loops.java
@@ -0,0 +1,17 @@
+package ConceptBuilding;
+
+public class Loops
+{
+    public static void main(String[] args) {
+        int x = 5;
+
+        for (int i = 0; i<x; i++)
+        {
+            for (int j=0;j<x;j++)
+            {
+                System.out.println(x);
+            }
+
+        }
+    }
+}
