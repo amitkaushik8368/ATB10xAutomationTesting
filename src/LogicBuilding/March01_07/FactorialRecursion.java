b7d8a386eeb59144dcc2cd6f64387b4a9489b5e2
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-03-02 22:07:14 +0530

    02 March 2025 Night Commit

---
+++ src/LogicBuilding/March01_07/FactorialRecursion.java
@@ -0,0 +1,18 @@
+package LogicBuilding.March01_07;
+
+public class FactorialRecursion
+{
+
+    public static void main(String[] args)
+    {
+        int num = 17;
+        int factorial = 1;
+        while (num>0)
+        {
+            factorial = factorial*num--;
+
+        }
+        System.out.println(factorial);
+    }
+
+}
