55af51929d6c12ffba32feee4a170b0d8173117e
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-03-07 22:49:43 +0530

    07 March 2025 Night Commit

---
+++ src/Interview/Interview0703.java
@@ -0,0 +1,20 @@
+package Interview;
+
+public class Interview0703 {
+    public static void main(String[] args) {
+        String str = "My name is Amit";
+        //output = "yM eman si timA"
+        String [] strArray = str.split(" ");
+        StringBuilder strB = new StringBuilder();
+        for (String s : strArray)
+        {
+            StringBuilder stringBuilder = new StringBuilder(s);
+            stringBuilder.reverse();
+            strB.append(stringBuilder).append(" ");
+        }
+
+        System.out.println(strB);
+
+    }
+
+}
