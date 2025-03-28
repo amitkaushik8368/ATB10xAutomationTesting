2beac10005f1c7b92cb52a7052d3842e5c1bf406
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-03-28 09:50:22 +0530

    28 March 2025 Morning Commit

--- src/CodingQuestionsTarget_100/LabNo_019.java
+++ src/CodingQuestionsTarget_100/LabNo_019.java
@@ -1,5 +1,7 @@
 package CodingQuestionsTarget_100;
 
+import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -13,9 +15,21 @@ public class LabNo_019
         linkHashMap.put(3, "Rudra Kaushik");
         linkHashMap.put(4, "Pratima Kaushik");
 
-        linkHashMap.forEach(
-                (key, value) -> System.out.println(key + "->" + value)
-        );
+//        linkHashMap.forEach(
+//                (key, value) -> System.out.println(key + "->" + value)
+//        );
+
+//        for (Map.Entry<Integer, String> entry : linkHashMap.entrySet())
+//        {
+//            System.out.println(entry.getKey() + "->"  + entry.getValue());
+//        }
+
+        Iterator<Map.Entry<Integer, String>> iterator = linkHashMap.entrySet().iterator();
+        while (iterator.hasNext())
+        {
+            iterator.next();
+        }
+
 
     }
 }
