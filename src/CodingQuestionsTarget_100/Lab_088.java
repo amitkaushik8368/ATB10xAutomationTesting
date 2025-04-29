068467bed7ca000cdf827928cefd5f458170a5df
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-04-29 21:56:49 +0530

    29 April 2025 Evening Commit

---
+++ src/CodingQuestionsTarget_100/Lab_088.java
@@ -0,0 +1,19 @@
+package CodingQuestionsTarget_100;
+
+public class Lab_088
+{
+    public static void main(String[] args) {
+
+        /*
+
+                **
+               *  *
+               *  *
+                **
+
+
+         */
+
+
+    }
+}
