f2a4af19aae78e9b5f82871ac304b7bfe3ec0de0
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-05-07 14:16:35 +0530

    07 May 2025 Afternoon Commit

---
+++ src/CodingQuestionsTarget_100/Lab_091.java
@@ -0,0 +1,9 @@
+package CodingQuestionsTarget_100;
+
+public class Lab_091
+{
+    public static void main(String[] args)
+    {
+        String str = "You are good person you should be proud of yourself";
+    }
+}
