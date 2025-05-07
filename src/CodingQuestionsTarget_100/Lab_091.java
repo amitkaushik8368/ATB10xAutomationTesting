20d6ac1f4e4ec2e9045fdbc8d5dce49527b605ee
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-05-07 14:35:58 +0530

    07 May 2025 Afternoon Commit2

--- src/CodingQuestionsTarget_100/Lab_091.java
+++ src/CodingQuestionsTarget_100/Lab_091.java
@@ -1,9 +1,27 @@
 package CodingQuestionsTarget_100;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Lab_091
 {
     public static void main(String[] args)
     {
         String str = "You are good person you should be proud of yourself";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String[] strArray = str.split("\\s+");
+        System.out.println(strArray.length);
+        for (String s: strArray)
+        {
+            if (s.equalsIgnoreCase("you"))
+            {
+                String x = s.toUpperCase();
+               // System.out.println(x);
+                stringBuilder.append(x).append(" ");
+            } else stringBuilder.append(s).append(" ");
+        }
+        System.out.println(stringBuilder);
+
     }
 }
